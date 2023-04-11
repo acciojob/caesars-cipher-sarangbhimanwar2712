6110ca0f5be3a9624c1a22a6b5f0d74f467751cd
Sarang Sanjay Bhimanwar <117634221+sarangbhimanwar2712@users.noreply.github.com>
2023-04-11 21:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,38 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   // let decodedArr = []; // Your Result goes here
   // Only change code below this line
+// define the alphabet and ROT13 alphabet
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const rot13Alphabet = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
 
-  return ;//return decodedArr
+  // convert the input string to an array of characters
+  const chars = encodedStr.split('');
+
+  // iterate over each character in the array
+  const decodedChars = chars.map(char => {
+    // check if the character is alphabetic
+    const isAlphabetic = alphabet.includes(char);
+
+    if (isAlphabetic) {
+      // get the index of the character in the alphabet
+      const index = alphabet.indexOf(char);
+
+      // find the corresponding character in the ROT13 alphabet
+      const decodedChar = rot13Alphabet[index];
+
+      return decodedChar;
+    } else {
+      // pass through non-alphabetic characters
+      return char;
+    }
+  });
+
+  // join the array of characters back into a string
+  const decodedStr = decodedChars.join('');
+
+  return decodedStr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
